47d9bb6574384daf98679d64b9bdec6a79c90370
seongheum <seongheum@ssu.ac.kr>
2021-12-30 12:50:10 +0900

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 # 도커 이미지 만들기
 # - 도커 이미지명과 태그명을 적절하게 수정 (필요시)
 
+\cp -f docker_images/cfg/Dockerfile Dockerfile
 docker build --tag nia-ssp:0.6 .
+rm -f Dockerfile
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#str="docker run --name=NIA-SSP --runtime=nvidia --rm -v $PWD/test_datasets/data:/ssp/data -v $PWD/test_datasets/cfg:/ssp/cfg nia-ssp:0.5 python valid.py --datacfg data/$1/070308.data --modelcfg cfg/yolo-pose.cfg --weightfile data/$1/models/model.weights"
-str="docker run --name=NIA-SSP --runtime=nvidia --rm -v $PWD/test_datasets/data:/ssp/data -v $PWD/test_datasets/cfg:/ssp/cfg ssuvip/nia-ssp:latest python valid.py --datacfg data/$1/070308.data --modelcfg cfg/yolo-pose.cfg --weightfile data/$1/models/model.weights"
+str="docker run --name=NIA-SSP --runtime=nvidia --rm -v $PWD/test_datasets:/ssp/data -v nia-ssp:0.6 python valid.py --datacfg data/$1/070308.data --modelcfg data/$1/models/yolo-pose.cfg --weightfile data/$1/models/model.weights"
 echo $str
 eval $str
