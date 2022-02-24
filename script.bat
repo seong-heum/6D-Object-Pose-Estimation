0b8d6cfc6c8c5444ed0e3cc9631ed453216ccec3
seongheum <seongheum@ssu.ac.kr>
2022-02-24 09:51:18 +0900

    update

--- script.bat
+++ script.bat
@@ -1,6 +1,7 @@
 @echo on
-set ID="090109"
+set ID="070308"
 python docker_images/src/prepare.py %ID%
+python docker_images/src/check.py --datacfg data/%ID%/%ID%.data
 python docker_images/src/train.py --datacfg data/%ID%/%ID%.data --modelcfg data/%ID%/models/yolo-pose.cfg --initweightfile datasets/models/darknet19_448.conv.23 --pretrain_num_epochs 15
 :: python docker_images/src/train.py --datacfg data/%ID%/%ID%.data --modelcfg data/%ID%/models/yolo-pose.cfg --initweightfile data/%ID%/models/model.weights --pretrain_num_epochs 15
 :: python docker_images/src/valid.py --datacfg data/%ID%/%ID%.data --modelcfg data/%ID%/models/yolo-pose.cfg --weightfile data/%ID%/models/model.weights
